36d744ec22dafd47b877f9a0b024e1448c7c6513
v-giorgio <vitorlcardoso17@gmail.com>
2022-03-24 20:27:40 -0300

    feature: external api testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fetch = require("node-fetch-commonjs");
 require("dotenv").config();
 
 const port = process.env.PORT || 3001;
@@ -7,10 +8,24 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(port, () => {
+const testAPI = async () => {
+  try {
+    const response = await fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=${process.env.API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data["Time Series (Daily)"]["2022-03-24"]);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+testAPI();
+
+/* app.listen(port, () => {
   try {
     console.log(`Api running on ${port}`);
   } catch (e) {
     console.log(`Failed to run API:\n\n ${e}`);
   }
-});
+}); */
