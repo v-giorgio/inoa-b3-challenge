fd0043349e2021bca4f0ef829afabaa8c336a477
v-giorgio <vitorlcardoso17@gmail.com>
2022-03-24 21:46:55 -0300

    feature: AssetController is finished

--- backend/src/controllers/AssetController.js
+++ backend/src/controllers/AssetController.js
@@ -2,6 +2,7 @@ const APIController = require("./APIController");
 const Assets = require("../models/Assets");
 
 class AssetController {
+  /* check if asset is in the database */
   static getOneAsset = async (name, queryDate) => {
     try {
       const asset = await Assets.findOne({
@@ -14,18 +15,7 @@ class AssetController {
     }
   };
 
-  static createAsset = async (asset) => {
-    const assetData = asset;
-
-    try {
-      const newAsset = await Assets.create(assetData);
-
-      return res.status(200).json(newAsset);
-    } catch (e) {
-      return res.status(500).json(e.message);
-    }
-  };
-
+  /* return asset if it is in the database OR call the API method, post it and then return it */
   static findOrCreateAsset = async (req, res) => {
     const inputAsset = req.body;
 
@@ -38,11 +28,15 @@ class AssetController {
       if (asset) {
         return res.status(200).json(asset);
       }
-      /** TO DO:
-       *
-       *   Se não tiver o asset no banco, chamar o método da API e adicioná-lo usando createAsset
-       *
-       */
+
+      const assetApi = await APIController.getDataAPI(
+        inputAsset.name,
+        inputAsset.queryDate
+      );
+
+      const newAsset = await Assets.create(JSON.stringify(assetApi));
+
+      return res.status(200).json(newAsset);
     } catch (e) {
       return res.status(500).json(e.message);
     }
