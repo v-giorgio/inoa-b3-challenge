a7c4e72ce4bf372691a63644ff8d5c1d63716c68
v-giorgio <vitorlcardoso17@gmail.com>
2022-03-25 22:08:27 -0300

    feature: navbar component

---
+++ frontend/src/components/NavigationBar/styles.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const NavSection = styled.nav`
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 1.5%;
+`;
+
+export const NavLogo = styled.img`
+  width: 316px;
+  height: 118px;
+`;
