a0bd026f77639b9de47a783b77b6bdab37469183
v-giorgio <vitorlcardoso17@gmail.com>
2022-03-28 23:04:21 -0300

    feature: cors configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 require("dotenv").config();
 const routes = require("./routes");
+const cors = require("cors");
 
 require("./database");
 
 const port = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
